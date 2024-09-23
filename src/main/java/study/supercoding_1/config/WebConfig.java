9d3c728111212b2cfb6dd8f661b419bb26ec3cf7
유종화 <sola201@naver.com>
2024-09-23 13:19:06 +0900

    :wrench:feature/환경설정

---
+++ src/main/java/study/supercoding_1/config/WebConfig.java
@@ -0,0 +1,18 @@
+package study.supercoding_1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 스프링 서버 전역적으로 CORS 설정
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 클라이언트의 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 쿠키 전송을 허용
+    }
+}
