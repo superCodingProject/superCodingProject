db604d086809d7222201ddc1a2db3bae1dd3135d
유종화 <sola201@naver.com>
2024-09-23 12:39:49 +0900

    :building_construction:feature/프로젝트 초기 구조 세팅

---
+++ src/main/java/study/supercoding_1/SuperCoding1Application.java
@@ -0,0 +1,13 @@
+package study.supercoding_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCoding1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCoding1Application.class, args);
+    }
+
+}
