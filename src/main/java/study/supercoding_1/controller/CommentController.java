4b1fd9d81a353d524cb47db42bc30b5273989c5d
lsy28901 <tndus28901@gmail.com>
2024-09-23 18:56:58 +0900

    :sparkles:feature/Comment 등록 구현

--- src/main/java/study/supercoding_1/controller/CommentController.java
+++ src/main/java/study/supercoding_1/controller/CommentController.java
@@ -1,4 +1,28 @@
 package study.supercoding_1.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import study.supercoding_1.dto.AddCommentDTO;
+import study.supercoding_1.service.CommentService;
+
+@Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
+
+    @ResponseBody
+    @PostMapping("/comments")
+    @Operation(summary = "댓글 작성",description = "새로운 댓글을 작성합니다.")
+    public ResponseEntity<String> addComment(@RequestBody AddCommentDTO addCommentDTO){
+        commentService.addComment(addCommentDTO);
+        return ResponseEntity.ok("성공");
+    }
+
 }
---
+++ src/main/java/study/supercoding_1/dto/AddCommentDTO.java
@@ -0,0 +1,9 @@
+package study.supercoding_1.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AddCommentDTO {
+    private String content;
+    private String author;
+}
