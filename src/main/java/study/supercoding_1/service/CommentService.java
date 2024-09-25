aac3404772b4fd95948f833f1d0087fecb0a76a7
lsy28901 <tndus28901@gmail.com>
2024-09-25 14:45:13 +0900

    :recycle: refactor:addComment 로직 수정

--- src/main/java/study/supercoding_1/service/CommentService.java
+++ src/main/java/study/supercoding_1/service/CommentService.java
@@ -1,21 +1,35 @@
 package study.supercoding_1.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import study.supercoding_1.dto.AddCommentDTO;
 import study.supercoding_1.entity.Comment;
+import study.supercoding_1.entity.Posts;
 import study.supercoding_1.repository.CommentRepository;
+import study.supercoding_1.repository.PostsRepository;
+import study.supercoding_1.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-    private final CommentRepository commentRepository;
 
-    public void addComment(AddCommentDTO addCommentDTO){
+    private final CommentRepository commentRepository;
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Comment addComment(AddCommentDTO addCommentDTO){
+
+        Posts post = postsRepository.findById(Integer.valueOf(addCommentDTO.getPost_id()))
+                .orElseThrow(()->new RuntimeException("server error"));
+
         Comment newComment = Comment.builder()
                 .content(addCommentDTO.getContent())
                 .author(addCommentDTO.getAuthor())
+                .post(post)
                 .build();
-        commentRepository.save(newComment);
+
+        return commentRepository.save(newComment);
     }
 }
