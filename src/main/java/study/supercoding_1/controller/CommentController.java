cc2536deae41aa9a7873cef2a7537f423644e6f7
lsy28901 <tndus28901@gmail.com>
2024-09-25 21:18:34 +0900

    :sparkles: feature:댓글 수정, 삭제구현

--- src/main/java/study/supercoding_1/controller/CommentController.java
+++ src/main/java/study/supercoding_1/controller/CommentController.java
@@ -3,34 +3,43 @@ package study.supercoding_1.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import study.supercoding_1.dto.AddCommentDTO;
-import study.supercoding_1.dto.GetCommentResponse;
+import study.supercoding_1.dto.*;
 import study.supercoding_1.service.CommentService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
-    @ResponseBody
     @PostMapping("/comments")
     @Operation(summary = "댓글 작성",description = "새로운 댓글을 작성합니다.")
-    public ResponseEntity<String> addComment(@RequestBody AddCommentDTO addCommentDTO){
-        commentService.addComment(addCommentDTO);
+    public ResponseEntity<String> addComment(@RequestBody AddCommentRequest addCommentRequest){
+        commentService.addComment(addCommentRequest);
         return ResponseEntity.ok("성공");
     }
 
-    @ResponseBody
+
     @GetMapping("/comments")
     @Operation(summary = "전체 댓글 조회",description = "전체 댓글을 조회합니다.")
     public ResponseEntity<List<GetCommentResponse>> getCommentList(){
         return ResponseEntity.ok(commentService.getCommentList());
     }
 
+    @PutMapping("/comments/{comment_id}")
+    @Operation(summary = "댓글 수정",description = "댓글 하나를 수정합니다.")
+    public ResponseEntity<UpdateCommentResponse> updateComment(@PathVariable(name = "comment_id") Long commentId,
+                                                               @RequestBody UpdateCommentRequest request){
+        return ResponseEntity.ok(commentService.updateComment(commentId,request));
+    }
+
+    @DeleteMapping("/comments/{comment_id}")
+    @Operation(summary = "댓글 삭제",description = "댓글 하나를 삭제합니다.")
+    public ResponseEntity<DeleteCommentResponse> deleteComment(@PathVariable(name = "comment_id") Long commentId){
+        return ResponseEntity.ok(commentService.deleteComment(commentId));
+    }
 
 }
