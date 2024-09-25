71c05e7ba2536b61998152f59d1ce2a6468246b8
lsy28901 <tndus28901@gmail.com>
2024-09-25 23:44:53 +0900

    :bug: fix:AddCommentRequest에서 post_id를 받아올 수 없었던 문제 해결

--- src/main/java/study/supercoding_1/dto/AddCommentRequest.java
+++ src/main/java/study/supercoding_1/dto/AddCommentRequest.java
@@ -1,12 +1,17 @@
 package study.supercoding_1.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 public class AddCommentRequest {
     private String content;
     private String author;
-    private int post_id;
+    @JsonProperty("post_id")
+    private int postId;
 }
