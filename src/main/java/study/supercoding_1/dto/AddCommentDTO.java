952598e27c33d1d41641bf12c64a04025dcaa510
lsy28901 <tndus28901@gmail.com>
2024-09-25 14:36:42 +0900

    :recycle: refactor:AddCommentDTO @Getter 추가

--- src/main/java/study/supercoding_1/dto/AddCommentDTO.java
+++ src/main/java/study/supercoding_1/dto/AddCommentDTO.java
@@ -1,9 +1,12 @@
 package study.supercoding_1.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class AddCommentDTO {
     private String content;
     private String author;
+    private String post_id;
 }
