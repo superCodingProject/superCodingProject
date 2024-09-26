23b8bdbf0a819daa5150cdde43a4ce1a73879d45
lsy28901 <tndus28901@gmail.com>
2024-09-26 15:01:06 +0900

    :recycle: refactor: 댓글 생성, 수정시 검증처리

--- src/main/java/study/supercoding_1/dto/AddCommentRequest.java
+++ src/main/java/study/supercoding_1/dto/AddCommentRequest.java
@@ -1,8 +1,7 @@
 package study.supercoding_1.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 public class AddCommentRequest {
+    @NotBlank(message = "댓글 내용을 입력해주세요")
+    @Size(min = 2, message = "내용은 2글자 이상이어야 합니다.")
     private String content;
+    @NotBlank(message = "댓글 작성자를 입력해주세요")
     private String author;
     @JsonProperty("post_id")
     private int postId;
--- src/main/java/study/supercoding_1/dto/UpdateCommentRequest.java
+++ src/main/java/study/supercoding_1/dto/UpdateCommentRequest.java
@@ -1,5 +1,8 @@
 package study.supercoding_1.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateCommentRequest {
+    @NotBlank(message = "수정할 내용을 입력해주세요")
+    @Size(min = 2, message = "내용은 2글자 이상이어야 합니다.")
     private String content;
 }
