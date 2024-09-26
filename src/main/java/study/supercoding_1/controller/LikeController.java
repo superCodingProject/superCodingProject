4e2cd94415800dca51671080d077e5ec9f47c536
유종화 <sola201@naver.com>
2024-09-27 00:55:42 +0900

    :sparkles: feature: LikeController 좋아요 추가 삭제 api 주소 작성

--- src/main/java/study/supercoding_1/controller/LikeController.java
+++ src/main/java/study/supercoding_1/controller/LikeController.java
@@ -1,11 +1,12 @@
 package study.supercoding_1.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import study.supercoding_1.dto.LikeCountResponse;
+import study.supercoding_1.dto.LikeRequest;
+import study.supercoding_1.dto.LikeResponse;
 import study.supercoding_1.service.LikeService;
 
 @RestController
@@ -16,8 +17,14 @@ public class LikeController {
     private final LikeService likeService;
 
     @GetMapping("/like/{post_id}")
-    public ResponseEntity<Integer> getPostLikeCount(@PathVariable(name = "post_id") int postId) {
-        Integer response = likeService.getPostLikeCount(postId);
+    public ResponseEntity<LikeCountResponse> getPostLikeCount(@PathVariable(name = "post_id") int postId) {
+        LikeCountResponse response = likeService.getPostLikeCount(postId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/like/{post_id}")
+    public ResponseEntity<LikeResponse> addOrCancelLike(@PathVariable(name = "post_id") int postId, @RequestBody LikeRequest request) {
+        LikeResponse response = likeService.addOrCancelLike(postId, request);
         return ResponseEntity.ok(response);
     }
 
