88ada139e0b8c3c1cb274020a36d078f91ee4ed5
유종화 <sola201@naver.com>
2024-09-26 22:38:32 +0900

    :sparkles: feature: LikeService 좋아요 갯수 찾는 메서드 작성

--- src/main/java/study/supercoding_1/service/LikeService.java
+++ src/main/java/study/supercoding_1/service/LikeService.java
@@ -1,15 +1,29 @@
 package study.supercoding_1.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import study.supercoding_1.entity.Post;
+import study.supercoding_1.exception.errorcode.CommonErrorCode;
+import study.supercoding_1.exception.exception.PostException;
 import study.supercoding_1.repository.LikeRepository;
+import study.supercoding_1.repository.PostRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class LikeService {
 
     private final LikeRepository likeRepository;
+    private final PostRepository postRepository;
 
+    public Integer getPostLikeCount(int postId) {
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(CommonErrorCode.POST_NOT_FOUND));
+        log.info(String.valueOf(post.getLikeCount()));
+        return post.getLikeCount();
+    }
 }
