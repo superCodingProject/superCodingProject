6378e0a852667aa894a8beda959c03ac0a4a36b7
유종화 <sola201@naver.com>
2024-09-26 21:42:38 +0900

    :sparkles: feature: 좋아요 엔티티 작성

---
+++ src/main/java/study/supercoding_1/entity/Like.java
@@ -0,0 +1,28 @@
+package study.supercoding_1.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "likes")
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+}
