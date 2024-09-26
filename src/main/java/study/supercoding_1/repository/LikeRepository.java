7d4901d1175e0e2ce49c826474bd98098d0ba582
유종화 <sola201@naver.com>
2024-09-27 00:54:50 +0900

    :sparkles: feature: LikeRepository 유저와 해당글을 찾기 위한 쿼리메서드 작성

--- src/main/java/study/supercoding_1/repository/LikeRepository.java
+++ src/main/java/study/supercoding_1/repository/LikeRepository.java
@@ -2,6 +2,11 @@ package study.supercoding_1.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import study.supercoding_1.entity.Like;
+import study.supercoding_1.entity.Post;
+import study.supercoding_1.entity.User;
+
+import java.util.Optional;
 
 public interface LikeRepository extends CrudRepository<Like, Integer> {
+    Optional<Like> findByUserAndPost(User user, Post post);
 }
