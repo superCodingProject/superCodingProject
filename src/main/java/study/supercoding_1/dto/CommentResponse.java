e9bc643c49a2ba51547c230869f9f9fdb9c311ec
lsy28901 <tndus28901@gmail.com>
2024-09-26 17:43:13 +0900

    :memo: docs:request, response 객체 문서화

--- src/main/java/study/supercoding_1/dto/CommentResponse.java
+++ src/main/java/study/supercoding_1/dto/CommentResponse.java
@@ -1,11 +1,12 @@
 package study.supercoding_1.dto;
 
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class CommentResponse {
+    @Schema(description = "댓글 CRUD 성공 메시지",example = "댓글이 성공적으로 작성되었습니다.")
     private String message;
 }
--- src/main/java/study/supercoding_1/dto/UpdateCommentRequest.java
+++ src/main/java/study/supercoding_1/dto/UpdateCommentRequest.java
@@ -1,5 +1,6 @@
 package study.supercoding_1.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,5 +14,6 @@ import lombok.NoArgsConstructor;
 public class UpdateCommentRequest {
     @NotBlank(message = "수정할 내용을 입력해주세요")
     @Size(min = 2, message = "내용은 2글자 이상이어야 합니다.")
+    @Schema(description = "수정할 댓글 내용",example = "이 게시물을 삭제해주세요!")
     private String content;
 }
